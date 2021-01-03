d36f20f10d41d88a992d699db68a40cbfd02a548
Misha Kushka <kushka.misha@gmail.com>
2021-01-03 08:44:09 +0200

    Setup for many signers

--- query.js
+++ query.js
@@ -9,8 +9,14 @@ module.exports = async (callback) => {
 
     const destination = '0x909Ae0dDf1ACaA3ccf32344922AE016Ab2558cBa';
     const senderAddr = '0x7403ab40723898eCf2450467Ba620EF7B77A6961';
-    const senderPrKey = 'deca899581d68b6b2ac88149e951afdd8f89666b95aa54a790c6d56d831ab718';
-    const prKeyBuffer = Buffer.from(senderPrKey, 'hex');
+    const prKeyBuffers = [
+      Buffer.from('deca899581d68b6b2ac88149e951afdd8f89666b95aa54a790c6d56d831ab718', 'hex'),
+    ];
+    // const prKeyBuffer = [
+    //   Buffer.from('deca899581d68b6b2ac88149e951afdd8f89666b95aa54a790c6d56d831ab718', 'hex'),
+    //   Buffer.from('24d52ea1a035de7a61fd0c706de4b095adca0c8193f1395140356b3df31b5b6a', 'hex'),
+    //   Buffer.from('5b07bae6366e7ca82f82989e9d18a5aeb6320234aa58a552a609677c95258278', 'hex'),
+    // ]
     const value = 0; // web3.utils.toWei('0.01', 'ether');
     const tokenAmount = 10;
     const nonce = (await multisig.nonce()).toString();
@@ -20,18 +26,25 @@ module.exports = async (callback) => {
       instance: tokenInstance,
       sender: senderAddr,
       recipient: destination,
-      amount: tokenAmount
+      amount: tokenAmount,
     });
 
-    const { r, s, v } = sign({
-      contractAddr: multisig.address,
-      destination: token.address,
-      value,
-      data,
-      nonce,
-      prKeyBuffer
-    });
-    console.log({ r, s, v });
+    const rs = [];
+    const ss = [];
+    const vs = [];
+    for (let i = 0; i < prKeyBuffers.length; i++) {
+      const { r, s, v } = sign({
+        contractAddr: multisig.address,
+        destination: token.address,
+        value,
+        data,
+        nonce,
+        prKeyBuffer: prKeyBuffers[i],
+      });
+      rs.push(r);
+      ss.push(s);
+      vs.push(v);
+    }
 
     // Allow MultiSig to spend tokens using transferFrom
     await tokenInstance.methods
@@ -47,7 +60,7 @@ module.exports = async (callback) => {
     console.log(`BasicToken.balanceOf(senderAddr) is ${await tokenInstance.methods.balanceOf(senderAddr).call()}`);
 
     // console.log(`Contract balance before: ${web3.utils.fromWei(await web3.eth.getBalance(multisig.address))} ETH`);
-    await multisig.execute(token.address /*destination*/, 0/*value*/, data, [r], [s], [v]);
+    await multisig.execute(token.address /*destination*/, 0/*value*/, data, rs, ss, vs);
     // console.log(`Contract balance after: ${web3.utils.fromWei(await web3.eth.getBalance(multisig.address))} ETH`);
 
     console.log(`BasicToken.balanceOf(destination) is ${await tokenInstance.methods.balanceOf(destination).call()}`);
