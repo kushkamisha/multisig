07968cd572c586ddcb319191429d12e5d72af94a
Misha Kushka <kushka.misha@gmail.com>
2022-01-08 19:17:47 +0000

    Update deps

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.7.5;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
 
 contract MultiSig {
     uint256 public nonce;
--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const BasicToken = artifacts.require("BasicToken");
-const MultiSig = artifacts.require("MultiSig");
-
-module.exports = function (deployer) {
-  deployer.deploy(BasicToken, 1000);
-  const owners = ["0xEc6751439B9cC8a13c61B7c73A4081eBc08be42a", "0xF0723672096c5dc5718aFB3530CBf951aAa9E4c7", "0x431C7C93533e5C88C9C499EF3c39D0BbE1A0A3C6"]
-  const threshold = owners.length - 1;
-  deployer.deploy(MultiSig, owners, threshold);
-};
